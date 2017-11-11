bc0307843de3894fb638f206ac065abb96ffe266
Al <alstruamann@gmail.com>
2017-11-10 20:31:54 -0600

    Edited pacman png file to use borders for light bikes in Tron.
    Updated Bikes class to contain both Computer and User class. Working on comp AI implementation.
    Game.py contains most necessary components to run the game.

--- Bikes.py
+++ Bikes.py
@@ -1,32 +1,110 @@
+import random
+
+#Values for Orange PNG start at 240
+#Values for player start at 230ish
+
 class User:
-    def __init__(self, pos, char):
+    def __init__(self, pos, char, diff=-1):
         self.x = pos[0]
         self.y = pos[1]
         self.path = []
         self.char = char
-        self.prev_char =None
+        self.prev_char = None
+        self.prev_move = None
         self.old = None
+        self.diff = diff
+    
+        self.CHAR_START = 240 #USER char repr
+        if diff == -1:
+            self.CHAR_START = 228 #CORRUPTION char repr
     
     def __str__(self):
         return self.char
 
+    def pos(self):
+        return (self.x, self.y)
+
     def move(self, direct):
         self.old = (self.x, self.y)
+        self.path += [self.old]
         if direct == "NORTH":
-            self.prev_char = '|'
             self.y -= 1
         elif direct == "SOUTH":
-            self.prev_char = '|'
             self.y += 1
         elif direct == "WEST":
-            self.prev_char = '-'
             self.x -= 1
         elif direct == "EAST":
-            self.prev_char = '-'
             self.x += 1
+        self.det_prev_char(direct)
+
+
     def __eq__(self, other):
         if isinstance(other, self.__class__):
             return self.x == other.x and self.y == other.y
         return False
-
     
+    def derezzed(self):
+        return self.path
+#I will need to implement an algorithm for the corruption to utilize when making their decisions.
+#A suggested implementation is to use a minimax algorithm. https://www.sifflez.org/misc/tronbot/
+    def det_prev_char(self, move):
+        if self.prev_move == None:
+            if move == "NORTH" or move == "SOUTH":
+                self.prev_char = chr(self.CHAR_START)
+            else:
+                self.prev_char = chr(self.CHAR_START + 1)
+        else:
+            index = 0
+            if self.prev_move == "EAST":
+                if move == "NORTH":
+                    index = 2
+                elif move == "SOUTH": 
+                    index = 5
+                else:
+                    index = 1
+            elif self.prev_move == "WEST":
+                if move == "NORTH":
+                    index = 3
+                elif move == "SOUTH": 
+                    index = 4
+                else:
+                    index = 1
+            elif self.prev_move == "NORTH":
+                if move == "WEST":
+                    index = 5
+                elif move == "EAST": 
+                    index = 4
+                else:
+                    index = 0
+            elif self.prev_move == "SOUTH":
+                if move == "WEST":
+                    index = 2
+                elif move == "EAST": 
+                    index = 3
+                else:
+                    index = 0
+            self.prev_char = chr(self.CHAR_START + index)
+        self.prev_move = move
+#Node is the map at a given point in time
+
+class Computer(User):
+    def make_move(self, MAP, MAP_WIDTH, MAP_HEIGHT):
+        if self.diff == 0:
+            self.random(MAP, MAP_WIDTH, MAP_HEIGHT)
+        elif self.diff == 1:
+            print "TODO"
+        elif self.diff == 2:
+            print "TODO"
+
+    def random(self, MAP, MAP_WIDTH, MAP_HEIGHT):
+        x = random.randint(0, 4)
+        if x == 0:
+            self.move("NORTH")
+        elif x == 1:
+            self.move("SOUTH")
+        elif x == 2:
+            self.move("WEST")
+        elif x == 3:
+            self.move("EAST")
+
+        
--- game.py
+++ game.py
@@ -6,10 +6,9 @@ from CYLGame import MessagePanel
 from CYLGame import MapPanel
 from CYLGame import StatusPanel
 from CYLGame import PanelBorder
-from User import User
+from Bikes import *
 
-
-class AppleFinder(Game):
+class Tron(Game):
     MAP_WIDTH = 60
     MAP_HEIGHT = 25
     SCREEN_WIDTH = 60
@@ -19,19 +18,20 @@ class AppleFinder(Game):
     CHAR_WIDTH = 16
     CHAR_HEIGHT = 16
     GAME_TITLE = "TRON"
-    CHAR_SET = "terminal16x16_gs_ro.png"
+    CHAR_SET = "tron16x16_gs_ro.png"
     
     EMPTY = ' '
-    USER = User((0, MAP_HEIGHT/2), '@')
-    CORRUPT = User((MAP_WIDTH - 1, MAP_HEIGHT/2), '%')
 
     ENEMIES = 1 # start out with 1 staticly allocated. We can move onto random as before but get to that later
 
     def __init__(self, random):
+        self.random = random
         self.running = True
         self.enemies = self.ENEMIES       
-       
-        self.objects = []
+    
+        self.USER = None
+        self.CORRUPTION = []
+
         self.turns = 0
         self.level = 0
         self.msg_panel = MessagePanel(self.MSG_START, self.MAP_HEIGHT+1, self.SCREEN_WIDTH - self.MSG_START, 5)
@@ -47,10 +47,23 @@ class AppleFinder(Game):
                             border=PanelBorder.create(bottom="-"))
         self.panels += [self.map]
 
-        #self.map[(self.player_pos[0], self.player_pos[1])] = self.PLAYER
-        self.map[(self.USER.x, self.USER.y)] = self.USER.char
-        self.map[(self.CORRUPT.x, self.CORRUPT.y)] = self.CORRUPT.char
-       
+        self.place_bikes()
+        for i in self.CORRUPTION:
+            self.map[i.pos()] = i.char
+        self.map[self.USER.pos()] = self.USER.char
+    
+    def place_bikes(self):
+        for i in range(0, self.ENEMIES+1):
+            x = self.random.randint(0, self.MAP_WIDTH - 1)
+            y = self.random.randint(0, self.MAP_HEIGHT - 1)
+
+            if self.map[(x,y)] == self.EMPTY:
+                if i == self.ENEMIES:
+                    self.USER = User((x,y), chr(239))
+                else:
+                    self.CORRUPTION += [Computer((x,y), chr(234), self.level)]
+                
+
     def handle_key(self, key):
         self.turns += 1
          
@@ -65,7 +78,7 @@ class AppleFinder(Game):
         if key == "Q":
             self.running = False
             return
-        
+       
         self.map[self.USER.old] = self.USER.prev_char
 
         self.USER.x %= self.MAP_WIDTH
@@ -76,6 +89,27 @@ class AppleFinder(Game):
         else:   
             self.map[(self.USER.x, self.USER.y)] = self.USER.char
 
+    #    self.spread_corruption()
+        
+    def spread_corruption(self):
+        for i in range(0, self.ENEMIES):
+            cor = self.CORRUPTION[i]
+            print(cor.pos())
+            cor.make_move(self.map, self.MAP_WIDTH, self.MAP_HEIGHT)
+            
+            self.map[cor.old] = cor.prev_char
+            cor.x %= self.MAP_WIDTH
+            cor.y %= self.MAP_HEIGHT
+            if self.map[cor.pos()] != self.EMPTY:
+                for j in cor.derrezed():
+                    self.map[j] = self.EMPTY
+                cor = None
+                del self.CORRUPTION[i]
+                self.ENEMIES -= 1
+                if self.ENEMIES == 0:
+                    self.running = False
+            else:
+                self.map[cor.pos()] = cor.char
 
     def is_running(self):
         return self.running
@@ -111,4 +145,4 @@ class AppleFinder(Game):
 
 if __name__ == '__main__':
     from CYLGame import run
-    run(AppleFinder)
+    run(Tron)
