65ed3f1d9564056f7b538e81c2b3cb69519836a8
Jonathan Beaulieu <123.jonathan@gmail.com>
2018-06-05 12:08:10 -0500

    Added working comp_sim

--- comp_sim.py
+++ comp_sim.py
@@ -6,21 +6,9 @@ from CYLGame.Database import GameDB
 from CYLGame.Comp import MultiplayerComp
 
 
-#
-# comp_token = sys.argv[1]
-# game = AppleFinder
-# compiler = Compiler()
-# gamedb = GameDB(sys.argv[2])
-# assert gamedb.is_comp_token(comp_token)
-#
-# sim_competition(compiler=compiler, game=game, gamedb=gamedb, token=comp_token, runs=100, debug=True)
-
-
-#def sim_multiplayer(s_token, gamedb, game, compiler, debug=False):
-
-s_token = 'S2ACBA9ED'
+c_token = sys.argv[1]
 game = Tron
 compiler = Compiler()
-gamedb = GameDB("temp_game")
-MultiplayerComp.sim_multiplayer(s_token, gamedb, game, compiler)
+gamedb = GameDB(sys.argv[2])
+MultiplayerComp.sim_comp(c_token, gamedb, game, compiler, debug=True)
 
